4db787456944e9c9c972c3db3bfefd65de8ac3e5
junqing <1551486328@qq.com>
2017-08-13 07:07:56 +0800

    HotModuleReplacement

--- webpack.config.js
+++ webpack.config.js
@@ -5,25 +5,27 @@ var WebpackNotifierPlugin = require('webpack-notifier');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ImageminPlugin = require('imagemin-webpack-plugin').default;
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var webpackDevServer = require('webpack-dev-server');
 
 var env = process.env.NODE_ENV || 'development';
 
 var definePlugin = new webpack.DefinePlugin({
-  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true'))
-})
+  __DEV__: JSON.stringify(JSON.parse(process.env.NODE_ENV || 'true'))
+});
 
 module.exports = {
   entry: {
     app: [
       // 'babel-polyfill',
       path.resolve(__dirname, 'script/app.js')
-    ]
+    ],
+    // vendor: ['pixi', 'p2', 'phaser', 'webfontloader']
   },
   devtool: 'source-map', //cheap-source-map
   output: {
     pathinfo: true,
-    path: path.resolve(__dirname, 'bin'),
-    publicPath: './bin/',
+    path: path.resolve(__dirname, './bin'),
+    // publicPath: './bin/',
     filename: 'js/bundle.js'
   },
   watch: true,
@@ -41,28 +43,41 @@ module.exports = {
       from: 'laya/assets/', to:"assets/"
     }]),
     new ImageminPlugin({
-      // disable: process.env.NODE_ENV !== 'production', // Disable during development
-      test: /\.(jpe?g|png)$/i,
-      /* pngquant: {
-        quality: '25-60'
-      }, */
+      disable: process.env.NODE_ENV !== 'production', // Disable during development
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      pngquant: {
+        quality: '25-45'
+      },
       optipng: {
-        optimizationLevel: 3
+        optimizationLevel: 1
       }
     }),
-    new BrowserSyncPlugin({
+    new webpack.HotModuleReplacementPlugin(),
+    /*new BrowserSyncPlugin({
       host: process.env.IP || 'localhost',
       port: process.env.PORT || 3000,
       server: {
         baseDir: ['./', './bin']
       }
-    })
+    }),*/
+    /*new webpack.optimize.UglifyJsPlugin({
+      drop_console: true,
+      minimize: true,
+      output: {
+        comments: false
+      }
+    }),*/
+    // new webpack.optimize.CommonsChunkPlugin({ name: 'vendor'/* chunkName= */, filename: 'vendor.bundle.js'/* filename= */})
   ],
   module: {
     rules: [
       { test: /\.js|jsx$/, use: ['babel-loader'], include: path.join(__dirname, 'script') },
       { test: /\.(jade|pug)$/, use: ['pug-loader?pretty=true'] }
     ]
+  },
+  devServer:{
+    hot: true,
+    contentBase: "./bin/",
   },
   resolve: {
     modules: [
@@ -74,4 +89,4 @@ module.exports = {
 
     }
   }
-}
+};
