332a785ef43b5d92bc140057535449cf2e769dc6
junqing <1551486328@qq.com>
2017-10-08 00:34:35 +0800

    merge

--- script/reducer/reducer.js
+++
@@ -1,9 +0,0 @@
-import counterReducer from "./counterReducer";
-import todosReducer from "./todosReducer";
-
-var { combineReducers } = Redux;
-
-export default combineReducers({
-	counter: counterReducer, // 键名就是该 reducer 对应管理的 state
-	todos: todosReducer
-});
