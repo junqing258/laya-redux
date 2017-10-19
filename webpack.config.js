30fa4708184c455f3246cb5929ae7b156ff59454
ZHELIHAO\ZHANGJUNQING <1551486328@qq.com>
2017-10-19 14:17:26 +0800

    config

--- webpack.config.js
+++ webpack.config.js
@@ -6,80 +6,88 @@ var ImageminPlugin = require('imagemin-webpack-plugin').default;
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var webpackDevServer = require('webpack-dev-server');
 
+//var zconfig = require("./zconfig");
+
 var env = process.env.NODE_ENV || 'development';
 
-console.log(env);
-
 var definePlugin = new webpack.DefinePlugin({
-  __DEV__: env==='development'? true: false
+    __DEV__: env!=="production"
 });
 
-module.exports = {
-  entry: {
-    app: [
-      // 'babel-polyfill',
-      path.resolve(__dirname, 'src/app.js')
-    ]
-  },
-  devtool: 'source-map', //cheap-source-map
-  output: {
-    pathinfo: true,
-    path: path.resolve(__dirname, './bin'),
-    // publicPath: './bin/',
-    filename: 'js/bundle.js'
-  },
-  watch: true,
-  plugins: [
+var plugins = [
     definePlugin,
-    new WebpackNotifierPlugin( {
-      excludeWarnings: true // alwaysNotify: false
-    }),
-    /*new CopyWebpackPlugin([{
-      from: 'laya/assets/', to:"assets/"
-    }]),
-    new ImageminPlugin({
-      disable: process.env.NODE_ENV !== 'production', // Disable during development
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      pngquant: {
-        quality: '25-45'
-      },
-      optipng: {
-        optimizationLevel: 1
-      }
-    }),*/
-    // new webpack.HotModuleReplacementPlugin(),
-    /*new BrowserSyncPlugin({
-      host: process.env.IP || 'localhost',
-      port: process.env.PORT || 3000,
-      server: {
-        baseDir: ['./', './bin']
-      }
-    }),*/
-    /*new webpack.optimize.UglifyJsPlugin({
-      drop_console: true,
-      minimize: true,
-      output: {
-        comments: false
-      }
-    }),*/
-    // new webpack.optimize.CommonsChunkPlugin({ name: 'vendor'/* chunkName= */, filename: 'vendor.bundle.js'/* filename= */})
-  ],
-  module: {
-    rules: [
-      { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') }
-    ]
-  },
-  devServer:{
-    hot: true,
-    contentBase: "./bin/",
-  },
-  resolve: {
-    modules: [
-      path.resolve('./src'),
-      path.resolve('./node_modules')
-    ], 
-    alias: {
+    new WebpackNotifierPlugin()
+];
 
-    }
+
+var plugins2 = [
+    new webpack.optimize.UglifyJsPlugin({
+        drop_console: true,
+        compress: false,
+        mangle: false,
+        output: {
+            comments: false,
+        },
+        beautify: false,
+        sourceMap: true,
+        test: /js\/.*\.js($|\?)/i
+    }),
+    new CopyWebpackPlugin([ {
+        from: 'bin/',
+        to: path.resolve("D:\\www\\files\\game\\byxxl"),
+        ignore: ["bin/index.html"]
+    } ]),
+    new ImageminPlugin({
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        // pngquant: { quality: '15-40' },
+        optipng: {
+        	optimizationLevel: 3
+      	} 
+    })
+];
+
+/* if (process.env.NODE_ENV === 'devserver') {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+} */
+if (process.env.NODE_ENV === 'production') {
+    plugins = plugins.concat(plugins2);
+}
+
+/**************************************************************************************************************************
+ **
+ */
+module.exports = {
+    entry: {
+        app: [  path.resolve(__dirname, 'src/app.js') ]
+    },
+    devtool: 'source-map',
+    output: {
+        pathinfo: true,
+        path: path.resolve(__dirname, './bin'), 
+        //path: path.resolve(zconfig.hp+"\\www\\files\\game\\byxxl"),
+        filename: 'js/bundle.js'
+    },
+    watch:  env!=="production",
+    plugins: plugins,
+    module: {
+        rules: [ { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') } ]
+    },
+    devServer: {
+        hot: false,
+        contentBase: "./bin/",
+        setup: function(app) {
+            app.get("/test", (req,res)=> {
+                res.json( require("bin/json/play.json") );
+            });
+        }
+    },
+    resolve: {
+        modules: [
+          path.resolve('./src'),
+          path.resolve('./node_modules')
+        ], 
+        alias: {
+
+        }
   }
 };
