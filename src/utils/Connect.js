c0d1849253eee17f5ea1cfb1c24a5eb3a06649fc
ZHELIHAO\ZHANGJUNQING <1551486328@qq.com>
2017-10-30 18:29:52 +0800

    connect

--- src/utils/Connect.js
+++ src/utils/Connect.js
@@ -1,36 +1,68 @@
+var subMap = {}, comStateTarget = {};
+var gid = 0;
 
-import { uuid, getIn } from 'utils/util';
+export default function Connect(path, component, bindState, order) {
+	order = (order || "D").toUpperCase();
+	let pathName = path/*.replace(/\./g, "_")*/;
 
+	if (!subMap[pathName]) {
+		subMap[pathName] = {};
+	}
+	if (!component.uuid) {
+		let uid = order+(gid++);
+		component.uuid = uid;
+		component.state = {};
+		component.preState = {};
+		component.bindState = {};
+	}
 
-var subList = {};
-export default function Connect(path, component, bindState) {
-	if (!component.uuid) { component.uuid = uuid(); }
-	if (!subList[path]) {  subList[path] = {}; } 
-	subList[path][component.uuid] = component;
-
-	bindState && (component.bindState = bindState);
-	
-	var _state = getIn(Connect.store, path);
-	component.state = _state;
-	if (typeof component.bindState === "function") { component.bindState(_state); }
+	let _state = getIn(path, Connect.store);
+	subMap[pathName][component.uuid] = component;
+	if (typeof bindState === "function") {
+		component.bindState[pathName] = bindState;
+		bindState(_state, undefined); 
+	}
+	component.state[pathName] = comStateTarget[pathName] = _state;
 }
 
-Connect.provider = store => {
+
+export function provider(store) {
 	if (Connect.store) { console.warn("store be used"); }
 	Connect.store = store;
-	var unsubscribe = store.subscribe(() => {
-		Object.keys(subList).forEach( path => {
-			Object.keys(subList[path]).forEach( uuid => {
-				let component = subList[path][uuid];
-				var _state = getIn(Connect.store, path);
-				if (/*component.displayedInStage===false || */component.destroyed) {
-					return delete subList[path][uuid];
+	Connect.unsubscribe = store.subscribe(() => {
+		Object.keys(subMap).forEach( path => {
+			let pathName = path/*.replace(/\./g, "_")*/;
+			let _state = getIn(pathName, Connect.store);
+			if (comStateTarget[pathName] === _state) return;
+			Object.keys(subMap[pathName]).forEach( uuid => {
+				let component = subMap[pathName][uuid];
+				if (!component||component.destroyed) return delete subMap[pathName][uuid];
+				if (typeof component.bindState[pathName] === "function") {
+					let bindState = component.bindState[pathName];
+					bindState.call(component, _state, component.state[pathName]);
 				}
-				if (component.state !== _state) {
-					if (typeof component.bindState === "function") { component.bindState(_state, component.state); }
-					component.state = _state;
-				}
+				component.preState[pathName] = component.state[pathName];
+				component.state[pathName] = _state;
 			});
+			comStateTarget[pathName] = _state;
 		});
 	});
 };
+
+
+
+export function getIn (path, store) {
+	store = store || Connect.store;
+	if (typeof path!== "string") {
+		let t = store;
+		store = path;
+		path = t;
+	}
+	var paths = path.split('.');
+	var obj = typeof store.getState === "function"? store.getState(): store;
+	for (let i = 0,len = paths.length; i < len; i++) {
+	  obj = obj[paths[i]];
+	  if (obj === undefined) break;
+	}
+	return obj;
+}
