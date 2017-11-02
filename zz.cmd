bc15fcac1ffd621125997221ad948db8eebf02dd
ZHELIHAO\ZHANGJUNQING <1551486328@qq.com>
2017-11-02 17:21:16 +0800

    txt

--- zz.cmd
+++ zz.cmd
@@ -1,2 +1,2 @@
-xcopy D:\Users\zhangjunqing\git\laya-es6-webpack\bin /d/e/y D:\Users\zhangjunqing\git\gameHall\www\files\game\laya-es6-webpack
+xcopy D:\Users\zhangjunqing\git\laya-es6-webpack\bin /d/e/y/exclude:exclude.txt D:\Users\zhangjunqing\git\gameHall\www\files\game\laya-es6-webpack
 xcopy D:\Users\zhangjunqing\git\laya-es6-webpack\bin\js /e/y D:\Users\zhangjunqing\git\gameHall\www\files\game\laya-es6-webpack\js
