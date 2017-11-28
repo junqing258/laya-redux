3205ecb27f7c16ef60e8d343b5c9d5f91c8cffca
junqing <1551486328@qq.com>
2017-11-28 16:17:15 +0800

    sense

--- src/utils/Connect.js
+++ src/utils/Connect.js
@@ -1,23 +1,28 @@
 var M = {}, comStateTarget = {}, T = {}, KS = [];
 var gid = 0;
-
+/**
+ *
+ * @param {string} path
+ * @param {object} component
+ * @param {function} bindState
+ * @param {boolean} once
+ * @param {string} order
+ */
 export default function connect(path, component, bindState, order) {
 	order = (order || "D").toUpperCase();
 	let pathName = path/*.replace(/\./g, "_")*/;
-
 	if (!M[pathName]) M[pathName] = {};
 	if (!component.uuid) Object.assign(component, {
-		uuid: order+(gid++),
+		uuid: order + (gid++),
 		reduxState: {},
 		reduxPreState: {},
 		bindState: {}
 	});
-
 	let _state = getIn(path, T.store);
 	M[pathName][component.uuid] = component;
 	if (typeof bindState === "function") {
 		component.bindState[pathName] = bindState;
-		bindState(_state, undefined); 
+		bindState(_state, undefined);
 	}
 	component.reduxState[pathName] = comStateTarget[pathName] = _state;
 	KS = Object.keys(M);
@@ -28,37 +33,37 @@ export function provider(store) {
 	if (T.store) { console.warn("store be used"); }
 	T.store = store;
 	T.unsubscribe = store.subscribe(() => {
-		KS.forEach( path => {
+		KS.forEach(path => {
 			let pathName = path/*.replace(/\./g, "_")*/;
 			let _state = getIn(pathName, T.store);
 			if (comStateTarget[pathName] === _state) return;
+			comStateTarget[pathName] = _state;
 			Object.keys(M[pathName]).forEach( uuid => {
 				let component = M[pathName][uuid];
-				if (!component||component.destroyed) return delete M[pathName][uuid];
+				if (!component || component.destroyed) return delete M[pathName][uuid];
 				let bindState = component.bindState[pathName];
 				if (typeof bindState === "function") bindState(_state, component.reduxState[pathName]);
 				component.reduxPreState[pathName] = component.reduxState[pathName];
 				component.reduxState[pathName] = _state;
 			});
-			comStateTarget[pathName] = _state;
 		});
 	});
 };
 
 
 
-export function getIn (path, store) {
+export function getIn(path, store) {
 	store = store || T.store;
-	if (typeof path!== "string") {
+	if (typeof path !== "string") {
 		let t = store;
 		store = path;
 		path = t;
 	}
 	var paths = path.split('.');
-	var obj = typeof store.getState === "function"? store.getState(): store;
-	for (let i = 0,len = paths.length; i < len; i++) {
-	  obj = obj[paths[i]];
-	  if (obj === undefined) break;
+	var obj = typeof store.getState === "function" ? store.getState() : store;
+	for (let i = 0, len = paths.length; i < len; i++) {
+		obj = obj[paths[i]];
+		if (obj === undefined) break;
 	}
 	return obj;
 }
--- src/utils/SenseManager.js
+++ src/utils/SenseManager.js
@@ -1,5 +1,5 @@
 
-let curSense, curRouter, routerList = [], componentList = [], hisRouters = [];
+let curSense, curRouter, routerList = [], componentList = [], hisRouters = [], lockedHash = true;
 export default class SenseManager {
     
     static reg(list) {
@@ -19,37 +19,51 @@ export default class SenseManager {
     static loadSense(router, payload, props) {
         let index = routerList.indexOf(router);
         if (index===-1) return console.warn(`${router} not registered`);
-        if (curRouter&&!curSense.actived) return console.warn(`${curRouter} not actived`);
+        if (curRouter && !curSense.actived) return console.warn(`${curRouter} not actived`);
         let preSense,
             preProto = componentList[index].prototype;
         preProto.actived = false;
-        let promise;
-        if (typeof preProto.willMount === "function") promise = preProto.willMount();
+        lockedHash = true;
+        let promiselist = [];
+        if (curRouter && typeof curSense.willUnMount === "function") {
+            let p1 = curSense.willUnMount();
+            if (p1 instanceof Promise) promiselist.push(p1);
+        };
+        if (typeof preProto.willMount === "function") {
+            let p2 =  preProto.willMount();
+            if (p2 instanceof Promise) promiselist.push(p2);
+        }
         if (typeof preProto.getAsset === "function" && !preProto.hasLoaded) {
             let asset = preProto.getAsset();
-            let promise2 = new Promise(resolve=> {
-                Laya.loader.load(asset, Laya.Handler.create(null, ()=> {
-                    preProto.hasLoaded = true;
-                    resolve();
-                }));
-            });
-            if (promise instanceof Promise) promise = Promise.all([promise, promise2]);
+            if (Array.isArray(asset) && asset.length>0) {
+                let p3 = new Promise(resolve=> {
+                    Laya.loader.load(asset, Laya.Handler.create(null, ()=> {
+                        preProto.hasLoaded = true;
+                        resolve();
+                    }));
+                });
+                promiselist.push(p3);
+            }
         }
-        if (promise instanceof Promise) {
+        if (promiselist.length>0) {
             // tiploadding.show();
-            promise.then(value=> this.changeSense(router));
+            Promise.all(promiselist).then(value=> {
+                this.changeSense(router);
+            });
         } else {
-            this.changeSense(router)
+            this.changeSense(router);
         }
     }
 
     static changeSense(router) {
         let index = routerList.indexOf(router);
-        let preSense = componentList[index].getInstance();
+        let PrComponent = componentList[index];
+        let preSense = PrComponent.getInstance();
+        let hasHased = PrComponent.hasHased;
         if (curRouter) {
-            Laya.timer.frameOnce(1, curSense, ()=> {
+            Laya.timer.frameOnce(1, preSense, ()=> {
+                if (hasHased) this.pushHistory(curRouter);
                 curSense.destroy();
-                this.pushHistory(curRouter);
                 curRouter = router;
                 curSense = preSense;
                 curSense.actived = true;
@@ -59,6 +73,8 @@ export default class SenseManager {
             curSense = preSense;
             curSense.actived = true;
         }
+        if (hasHased) location.hash = router;
+        setTimeout(()=> lockedHash = false, 110);
         Laya.stage.addChildAt(preSense, 0);
     }
 
@@ -76,4 +92,28 @@ export default class SenseManager {
         if (router) hisRouters.push(router);
     }
 
-} 
+}
+
+export function sense(router, hasHased) {
+    return function(target) {
+        let i = routerList.length;
+        routerList[i] = router;
+        let component = target;
+        component.hasHased = hasHased===false? false: true;
+        if (!component.getInstance) {
+            component.getInstance = function() {
+                if (!component.instance||component.instance.destroyed) component.instance = new component();
+                return component.instance;
+            }
+        }
+        componentList[i] = component;
+    }
+}
+
+window.addEventListener("hashchange", function() {
+    if (lockedHash) return;
+    let hash = location.hash;
+    if (hash!==curRouter) {
+        SenseManager.loadSense(hash.slice(1));
+    }
+});
