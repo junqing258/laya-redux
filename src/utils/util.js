021de4610a652294865444a7de2ed14adca86089
junqing <1551486328@qq.com>
2017-12-05 22:46:05 +0800

    util

--- src/utils/util.js
+++ src/utils/util.js
@@ -8,11 +8,35 @@ export function uuid () {
   return uuid;
 }
 
-export function random (max, min) {
+export function deepClone(values) {
+  var copy;
+  if ( null == values ||  "object" !=  typeof values)  return values;
+  if (values instanceof Date ) {
+      copy = new Date();
+      copy.setTime(values.getTime());
+      return copy;
+  }
+  if (values instanceof Array ) {
+      copy = [];
+      for (var i = 0, len = values.length; i < len; i++) {
+          copy[i] = deepClone(values[i]);
+      }
+      return copy;
+  }
+  if (values instanceof Object ) {
+      copy = {};
+      for (var attr in values) {
+          if (values.hasOwnProperty(attr)) copy[attr] = deepClone(values[attr]);
+      }
+      return copy;
+  }
+  throw new Error("Unable to copy values! Its type isn't supported.");
+}
+
+export function randomNum(max, min) {
   min = min || 0;
-  let tmax = Math.max(max, min);
-  let tmin = Math.min(max, min);
-  return tmin + Math.round(Math.random() * (tmax - tmin));
+  let tmax = Math.max(max, min), tmin = Math.min(max, min);
+  return Math.floor(min + Math.random() * (max - min));
 }
 
 /**
